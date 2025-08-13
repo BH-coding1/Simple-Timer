189540c6558bab5985acf0ea2fc3b7416a0cd16a
BH-coding1 <bhgumede21@gmail.com>
2025-08-13 19:04:50 +0200

    Delete CryptoDashBoard directory

--- CryptoDashBoard/src/components/FilterInput.jsx
+++
@@ -1,9 +0,0 @@
-const FilterInput = ({filter,onfilterChange,coins}) => {
-    return ( 
-    <div className="flex items-center  rounded-lg px-3 py-2 ">
-        <input className="bg-transparent text-white placeholder-gray-400 focus:outline-none w-200" 
-        type='text' placeholder="Filter by name or symbol" value={filter} onChange={(e) => onfilterChange(e.target.value)}/>
-    </div> );
-}
- 
-export default FilterInput;
